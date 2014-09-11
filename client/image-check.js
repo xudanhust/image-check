857847a5bb07abe9ccff72101d4cd24370e8dd2d
xudanhust <xudanhust@gmail.com>
2014-09-12 00:15:52 +0800

    update

---
+++ client/image-check.js
@@ -0,0 +1,35 @@
+'use strice';
+
+(function($){
+
+    var url = 'http://localhost:8888';
+
+    var imageCheck = function(){
+        var images = $('img');
+        images.each(function(){
+            var src = $(this).attr('src'),
+                testImg = new Image();
+            testImg.src = src;
+            testImg.onload = function(){
+                $.ajax({
+                    url: url,
+                    dataType: 'jsonp',
+                    data: {
+                        url: src
+                    }
+                })
+                .done(function(o) {
+                    if(o.fileSize){
+                        var oversize = (o.fileSize / 1024) > (testImg.width * testImg.height / 2250);
+                        oversize && $('img[src="'+ o.url +'"]').addClass('oversize');
+                    }
+                })
+            }
+        });
+    };
+
+    $(function(){
+        imageCheck();
+    });
+
+})(jQuery);
---
+++ server/index.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var urllib = require('url');
+
+var port = 8888;
+
+http.createServer(function(req, res) {
+    var params = urllib.parse(req.url, true);
+    if (params.query && params.query.callback && params.query.url) {
+        download(params.query.url, function(fileRes){
+            var data = {
+                url: params.query.url,
+                fileSize: fileRes.headers['content-length']
+            };
+            res.end(params.query.callback + '(' + JSON.stringify(data) + ')');
+        });
+    } else {
+        res.end(params.query.callback + '(' + JSON.stringify({
+            error: 'Invalid params'
+        }) + ')');
+    }
+}).listen(port, function() {
+    console.log('server is listening on port ' + port);
+});
+
+function download(url, callback) {
+    http.get(url, function(res) {
+        callback(res);
+    }).on("error", function() {
+        callback(null);
+    });
+}
